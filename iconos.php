d59ec8d6c3d4664ef4371a4a2fdd487964008079
Ziwen <ziwen544newiz@gmail.com>
2022-09-09 16:22:33 +0200

    iconos

---
+++ iconos.php
@@ -0,0 +1,39 @@
+<?php
+
+function $Imagens($archivos){
+    $Doc ="<img src='../path/doc.png alt=' '> ";
+    $Csv ="<img src='../path/csv.png alt=' '> ";
+    $Exe ="<img src='../path/exe.png alt=' '> ";
+    $Jpg ="<img src='../path/jpg.png alt=' '> ";
+    $Mp3 ="<img src='../path/mp3.png alt=' '> ";
+    $Mp4 ="<img src='../path/mp4.png alt=' '> ";
+    $Odt ="<img src='../path/odt.png alt=' '> ";
+    $Pdf ="<img src='../path/pdf.png alt=' '> ";
+    $Png ="<img src='../path/png.png alt=' '> ";
+    $Ppt ="<img src='../path/ppt.png alt=' '> ";
+    $Rar ="<img src='../path/rar.png alt=' '> ";
+    $Svg ="<img src='../path/svg.png alt=' '> ";
+    $Txt ="<img src='../path/txt.png alt=' '> ";
+    $Zip ="<img src='../path/zip.png alt=' '> ";
+
+    $SWITCHImages = pathinfo($archivos,PATHINFO_EXTENSION );
+
+    switch($SWITCHImages){
+        case 'doc'; echo $Doc; break;
+        case 'csv'; echo $Csv; break;
+        case 'exe'; echo $Exe; break;
+        case 'jpg'; echo $Jpg; break;
+        case 'mp3'; echo $Mp3; break;
+        case 'mp5'; echo $Mp4; break;
+        case 'odt'; echo $Odt; break;
+        case 'pdf'; echo $Pdf; break;
+        case 'png'; echo $Png; break;
+        case 'ppt'; echo $Ppt; break;
+        case 'rar'; echo $Rar; break;
+        case 'svg'; echo $Svg; break;
+        case 'txt'; echo $Txt; break;
+        case 'zip'; echo $Zip; break;
+    }
+
+}
+?>
---
+++ index.php
@@ -0,0 +1,70 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <ul>
+        <li>
+            <img src="path/doc.png"  id = "Doc" >
+             <p>Document</p>
+        </li>
+        <li>
+            <img src="path/csv.png" id="Csv">
+            <p>CSV FILE</p>
+        </li>
+        <li> 
+            <img src="path/jpg.png" id ="Jpg" >
+            <p>JPG</p>
+        </li>
+        <li>
+            <img src="path/png.png" id = "Png">
+            <p>PNG</p>
+        </li>
+        <li>
+            <img src="path/txt.png" id ="Txt">
+            <p>TXT</p>
+        </li>
+        <li>
+            <img src="path/ppt.png" id = "Ppt">
+            <p>ppt</p>
+        </li>
+        <li>
+            <img src="path/odt.png" id ="Odt">
+            <p>ODT</p>
+        </li>
+        <li>
+            <img src="path/pdf.png" id="Pdf">
+            <p>PDF</p>
+        </li>
+        <li>
+            <img src="path/zip.png" id="Zip">
+            <p>ZIP</p>
+        </li>
+        <li>
+            <img src="path/rar.png" id="Rar">
+            <p>RAR</p>
+        </li>
+        <li>
+            <img src="path/exe.png" id="Exe">
+            <p>EXE</p>
+        </li>
+        <li>
+            <img src="path/svg.png" id="Vsg">
+            <p>VSG</p>
+        </li>
+        <li>
+            <img src="path/mp3.png" id="Mp3">
+            <p>MP3</p>
+        </li>
+        <li>
+            <img src="path/mp4.png" id="Mp4">
+            <p>MP4</p>
+        </li>   
+   </ul>
+    
+    
+</body>
+</html>
